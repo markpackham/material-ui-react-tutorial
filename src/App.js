5cc2291d2d5157225337af03d4b55f3becf2822b
mark <markpackham1@gmail.com>
2020-10-12 15:38:13 +0100

    Add Save icon to button, https://youtu.be/vyJU9efvUtQ?t=563

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 function App() {
   return (
@@ -11,7 +12,12 @@ function App() {
           Learn Material UI in React from
           https://www.youtube.com/watch?v=vyJU9efvUtQ
         </h3>
-        <Button variant="contained" color="secondary">
+        <Button
+          startIcon={<SaveIcon />}
+          size="large"
+          variant="contained"
+          color="secondary"
+        >
           Hello World
         </Button>
         <img src={logo} className="App-logo" alt="logo" />
