7b17655e03adcb14bf3074bc02da435d72ca474e
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-10-12 15:28:41 +0100

    Learn Materail UI from https://www.youtube.com/watch?v=vyJU9efvUtQ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>
+          Learn Material UI in React from
+          https://www.youtube.com/watch?v=vyJU9efvUtQ
+        </h3>
+        <Button variant="contained" color="secondary">
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
