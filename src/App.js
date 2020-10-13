905e56cc040665dcc8e899f23e9a32b1efe3bdf1
mark <markpackham1@gmail.com>
2020-10-13 11:43:03 +0100

    adding Checkbox, https://youtu.be/vyJU9efvUtQ?t=788

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+
+function CheckboxExample() {
+  return <div>Test</div>;
+}
 
 function App() {
   return (
@@ -12,14 +19,15 @@ function App() {
           Learn Material UI in React from
           https://www.youtube.com/watch?v=vyJU9efvUtQ
         </h3>
-        <Button
-          startIcon={<SaveIcon />}
-          size="large"
-          variant="contained"
-          color="secondary"
-        >
-          Hello World
-        </Button>
+        <CheckboxExample />
+        <ButtonGroup variant="contained" size="large">
+          <Button startIcon={<SaveIcon />} color="primary">
+            Save
+          </Button>
+          <Button startIcon={<DeleteIcon />} color="danger">
+            Delete
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
